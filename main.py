d05fdddc5451a68ce61f4216299443587af920b2
Ojas Patil <punk00pp@gmail.com>
2025-05-28 21:06:15 +0530

    Update CORS configuration to allow requests from the new frontend URL

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Frontend URL
+    allow_origins=["https://teleradio.netlify.app"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
